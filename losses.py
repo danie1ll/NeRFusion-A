19fdf346fc0f35d926597b23bf2ac9c4e2351b12
Justinas Zaliaduonis <justinas.schwartz@gmail.com>
2024-06-27 20:50:56 +0200

    add depth loss

--- losses.py
+++ losses.py
@@ -45,16 +45,7 @@ class NeRFLoss(nn.Module):
         self.lambda_distortion = lambda_distortion
 
     def forward(self, results, target, **kwargs):
-        d = {}
-        d['rgb'] = (results['rgb']-target['rgb'])**2
-
-        o = results['opacity']+1e-10
-        # encourage opacity to be either 0 or 1 to avoid floater
-        d['opacity'] = self.lambda_opacity*(-o*torch.log(o))
-
-        if self.lambda_distortion > 0:
-            d['distortion'] = self.lambda_distortion * \
-                DistortionLoss.apply(results['ws'], results['deltas'],
-                                     results['ts'], results['rays_a'])
-
-        return d
+        return dict(
+            rgb=(results['rgb']-target['rgb']) ** 2,
+            depth=(results['depth']-target['depth']) ** 2,
+        )
